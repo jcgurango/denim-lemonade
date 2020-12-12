a9ab8739ca4d11ac29671ab2e2b594fa2d1ee0cb
JC Gurango <jc@jcgurango.com>
2020-12-12 20:01:25 +0800

    Update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name=denim --network=public -v `pwd`:/app -it node:12 bash -c "cd /app && yarn install && (yarn web & yarn server)"
+docker run --rm --name=denim --network=public -v `pwd`:/app -d node:12 bash -c "cd /app && yarn install && (yarn web & yarn server)" && docker logs -f denim
