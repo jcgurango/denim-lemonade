b8a266e6ba979b5aec75414a184c416ce0d0fc2c
JC Gurango <jc@jcgurango.com>
2021-05-17 06:06:55 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --name=denim --network=public -v `pwd`:/app -d node:12 bash -c "cd /app && yarn install && (yarn web & (ENABLE_SYNC=1 yarn server))"
+docker run --rm --name=denim --network=public -v `pwd`:/app -d node:12 bash -c "cd /app && yarn install && (yarn web & (ENABLE_SYNC=1 yarn server:dev))"
