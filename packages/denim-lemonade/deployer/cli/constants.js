fbc38c2cbea5298c5f41e0e1ff6a280d21db7f44
JC Gurango <jc@jcgurango.com>
2021-05-24 12:54:27 +0800

    Remove denim lemonade

--- packages/denim-lemonade/deployer/cli/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  loginTokenUrl: 'https://open.larksuite.com/miniprogram/api/v3/token',
-  loginRedirectUrl: 'https://open.larksuite.com/miniprogram/api/v3/passport/oauth/callback/',
-  loginPageUrl: 'https://passport.larksuite.com/suite/passport/page/login/?app_id=7&loginType=scan',
-  loginCheckUrl: 'https://open.larksuite.com/miniprogram/api/v3/check_login',
-};
