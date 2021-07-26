45d2f5c6a3ee7fad0ad24f0b6c65a178d9a00e83
JC Gurango <jc@jcgurango.com>
2021-07-26 14:07:49 +0800

    Add encoding

--- packages/denim-lemonade-server/src/sync/updaters/LarkUpdater.ts
+++ packages/denim-lemonade-server/src/sync/updaters/LarkUpdater.ts
@@ -83,7 +83,7 @@ export default class LarkUpdater extends LarkConnection {
 
             if (employee.mobile) {
               // Email already exists in the system.
-              const searchResult = await get('https://open.larksuite.com/open-apis/user/v1/batch_get_id?mobiles=' + employee.mobile);
+              const searchResult = await get('https://open.larksuite.com/open-apis/user/v1/batch_get_id?mobiles=' + encodeURIComponent(employee.mobile));
               
               if (!searchResult.code) {
                 const user = searchResult.data.mobile_users?.[employee.mobile];
